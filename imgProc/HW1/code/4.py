40846bbc7241948098f103b47c375aec1b406771
josh <joshdai930908@gmail.com>
2025-09-22 15:59:37 +0800

    imgProcess HW1

--- imgProc/HW1/code/4.py
+++ imgProc/HW1/code/4.py
@@ -1,6 +1,5 @@
 import cv2
 import os
-import utils
 
 os.makedirs("result/p4", exist_ok=True)
 
@@ -8,21 +7,24 @@ img = cv2.imread("src/Fig3.GIF", cv2.IMREAD_GRAYSCALE)
 if img is None:
     raise FileNotFoundError("Cannot open Fig3.GIF")
 
-binary = cv2.adaptiveThreshold(img, 255,
-                               cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                               cv2.THRESH_BINARY,
-                               29, 10)
-cv2.imwrite("result/p4/text_recovered1.bmp", binary)
-
-
 
 clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
 img_eq = clahe.apply(img)
 
-binary = cv2.adaptiveThreshold(img_eq, 255,
-                               cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                               cv2.THRESH_BINARY,
-                               35, 10)
+for b_size in range(11, 55, 2):
+    for C in range(10, 11, 1):
+        binary = cv2.adaptiveThreshold(img_eq, 255,
+                                       cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                       cv2.THRESH_BINARY,
+                                       b_size, C)
 
-cv2.imwrite("result/p4/text_recovered2.bmp", binary)
-print("Saved result to result/p4/text_recovered2.bmp")
+        # img_out = cv2.medianBlur(img_eq, b_size)
+        cv2.imwrite(f"result/p4/text_recovered_b{b_size}_C{C}.bmp", binary)
+
+# binary = cv2.adaptiveThreshold(img_eq, 255,
+#                                cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+#                                cv2.THRESH_BINARY,
+#                                11, 15)
+# cv2.imwrite("result/p4/text_recovered0.bmp", binary)
+
+print("Saved result to result/p4/text_recovered.bmp")

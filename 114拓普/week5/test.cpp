7f1757ad896150b68cdaa238f35388bddfe4aa0a
josh <joshdai930908@gmail.com>
2025-11-07 15:57:17 +0800

    KCIS w5

--- 114拓普/week5/test.cpp
+++ 114拓普/week5/test.cpp
@@ -1,17 +1,24 @@
+//Given n numbers, find the maximum sum of any contiguous subarray (elements in the array may be negative).
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
-    int arr[10005], n, mx=-1;
+    int arr[10005], n;
+    cin >>n;
     for(int i=0;i<n;++i)cin >>arr[i];
-    
-    for(int i=0;i<n;++i){
-        for(int j=1;j<n;++j){
 
+    int ma=-1e9, cur=0;
+    for(int i=0;i<n;++i){
+        for(int j=i;j<n;++j){
+            cur+=arr[j];
+            ma=max(ma, cur);
         }
+        cur=0;
     }
+
+    cout<<ma<<'\n';
 }
 /*
-13
-1 2 3 4 2 3 4 5 3 4 5 6 7
+7
+-2 1 -3 4 1 -1 5
 */

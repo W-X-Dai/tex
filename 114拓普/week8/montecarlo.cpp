3a455d73bafd37647ea331bd7c0f13f4299c0d52
josh <joshdai930908@gmail.com>
2025-12-05 11:56:07 +0800

    KCIS w8

--- 114拓普/week8/montecarlo.cpp
+++ 114拓普/week8/montecarlo.cpp
@@ -4,8 +4,12 @@ using namespace std;
 random_device rd;
 mt19937 g(rd());
 
+uniform_real_distribution<double> dist(0, 1);
+
 int main(){
     int n=1000, r=1000;
-
+    for(int i=0;i<n;++i){
+        cout<<dist(g)<<endl;
+    }
 
 }
